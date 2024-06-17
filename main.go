dffbc6ec32de2401fcd56f16c71608f7f1016e19
Siddhant <s1d.official011001011@gmail.com>
2024-06-17 20:01:45 +0530

    fix: cors issue fixed

--- main.go
+++ main.go
@@ -2,28 +2,23 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"mosaic/controllers"
-	"sync"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-type GridCell struct {
-	img image.Image
-}
-
-var (
-	ProcessedFiles = sync.Map{}
-)
-
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		BodyLimit: 1024 * 1024 * 1024,
+	})
+
+	app.Use(cors.New())
 
 	app.Use(logger.New())
 
-	app.Post("/cut", controllers.CutController)
+	app.Post("/mosaic", controllers.CutController)
 	app.Post("/start-overlay", controllers.StartOverlayController)
 
 	err := app.Listen(":3000")
